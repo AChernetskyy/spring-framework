76b971a2ff75f10979e361f9c1bae313e48be790
AChernatskyy <directvusa2010@gmail.com>
2022-03-23 09:14:03 -0400

    HTML

---
+++ Spring-09-HTML/src/main/java/com/example/spring09html/Spring09HtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring09html;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09HtmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09HtmlApplication.class, args);
+    }
+
+}
---
+++ Spring-09-HTML/src/test/java/com/example/spring09html/Spring09HtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring09html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09HtmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
